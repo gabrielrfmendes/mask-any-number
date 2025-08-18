82a9df003996491873e3d6f57b39e07ef4e77c26
Mendes <gabrielrfmendes@gmail.com>
2025-08-18 20:54:46 -0300

    chore: update package-lock.json and package.json for @rollup/plugin-terser integration; add UMD build configuration in rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
+import terser from "@rollup/plugin-terser";
 
 export default {
     input: "src/index.ts",
@@ -14,6 +15,19 @@ export default {
         {
             file: "dist/index.js",
             format: "esm",
+            sourcemap: true
+        },
+        {
+            file: "dist/index.umd.js",
+            format: "umd",
+            name: "maskNumber",
+            sourcemap: true
+        },
+        {
+            file: "dist/index.umd.min.js",
+            format: "umd",
+            name: "maskNumber",
+            plugins: [terser()],
             sourcemap: true
         }
     ],
